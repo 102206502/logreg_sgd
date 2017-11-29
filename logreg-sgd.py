304bb8bafc4feb990c281da82d4fde5ebb0780e8
102206502 <102206502@cc.ncu.edu.tw>
2017-11-29 17:55:20 +0800

    roc plote get confusion metrix, log_sgd error

--- logreg-sgd.py
+++ logreg-sgd.py
@@ -18,11 +18,9 @@ def load_train_test_data(train_ratio=.8):
     X = data.iloc[:,:8]
     X = numpy.concatenate((numpy.ones((len(X), 1)), X), axis=1)
     y = data.iloc[:,8]
-    print('X:\n', X)
-    print(X.shape)
-    print('y:\n', y)
-    import pdb; pdb.set_trace()  # breakpoint ee676081 //
-
+    # print('X:\n', X)
+    # print(X.shape)
+    # print('y:\n', y)
     return sklearn.model_selection.train_test_split(X, y, test_size = 1 - train_ratio, random_state=0)
 
 
@@ -40,15 +38,22 @@ def logreg_sgd(X, y, alpha = .001, iters = 100000, eps=1e-4):
     theta = numpy.zeros((d, 1))
     not_converge = True
     k = 0
+    lam = 0
 
     while not_converge:
-        func_g = numpy.matmul(X.T, (numpy.matmul(X, theta)-y)) + theta
-        theta_k = theta
-        theta = theta - alpha*func_g
         not_converge = False
+        for i in range(n):
+            x = X[i, :]
+            xT = numpy.transpose([x])
+            print('xT:\n', xT)
+            func_g = numpy.matmul(xT, (sigmoid(x, theta)-y[i+1]))
+            import pdb; pdb.set_trace()  # breakpoint 81437e0a //
+            
+            theta_k = theta
+            theta = theta - alpha*func_g
         for delta in abs(theta-theta_k):
-        	if delta > eps:
-        		not_converge = True
+            if delta > eps:
+                not_converge = True
         if k > iters:
             print('k > iters!')
             break
@@ -56,15 +61,29 @@ def logreg_sgd(X, y, alpha = .001, iters = 100000, eps=1e-4):
     # print('theta:\n', theta)
     return theta
 
+def sigmoid(X, theta):
+    y = 1/(1 + numpy.exp((numpy.dot(X, theta)*(-1))))
+    print(y.shape)
+    return y
 
 def predict(X, theta):
-    return 1/(1 + numpy.exp((numpy.dot(X, theta)*(-1))))
+    value = sigmoid(X, theta)
+    col, row = X.shape
+    y_hat = numpy.array(row).zeros()
+    for i, val in enumerate(value):
+        if val > 0.5:
+            y_hat[i] = 1
+        else:
+            y_hat[i] = 0
+    return y_hat
 
 # plot the ROC curve of your prediction
 # x aixes: TPR = TP / ( TP + FN )
 # y aixes: FPR = FP / ( FP + TN ) 
-def plot_roc_curve():
-	pass
+def plot_roc_curve(X_test, y_true, theta):
+    y_pred = predict(X_test, theta)
+    con_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred)
+    print(con_matrix)
 
 
 def main(argv):
@@ -72,10 +91,7 @@ def main(argv):
     X_train_scale, X_test_scale = scale_features(X_train, X_test, 0, 1)
 
     theta = logreg_sgd(X_train_scale, y_train)
-    y_hat = predict(X_train_scale, theta)
-    print("Linear train R^2: %f" % (sklearn.metrics.r2_score(y_train, y_hat)))
-    y_hat = predict(X_test_scale, theta)
-    print("Linear test R^2: %f" % (sklearn.metrics.r2_score(y_test, y_hat)))
+    plot_roc_curve(X_test, y_test, theta)
 
 
 if __name__ == "__main__":
