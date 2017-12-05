6561ff4955da210422ad3db5ee308dd3198f51f3
102206502 <102206502@cc.ncu.edu.tw>
2017-12-05 23:05:58 +0800

    update sgd, update tpr fpr 算法

--- logreg-sgd.py
+++ logreg-sgd.py
@@ -10,6 +10,7 @@ import sklearn.model_selection
 import sklearn.linear_model
 import sklearn.preprocessing
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
 
 def load_train_test_data(train_ratio=.8):
@@ -33,45 +34,38 @@ def scale_features(X_train, X_test, low=0, upp=1):
     return X_train_scale, X_test_scale
 
 # logreg_sgd with L1 regularization
-def logreg_sgd(X, y, alpha = .001, iters = 3, eps=1e-4):
+def logreg_sgd(X, y, alpha = .001, iters = 100000, eps=1e-4):
     # TODO: fill this procedure as an exercise
     n, d = X.shape
-
     theta = numpy.zeros((d, 1))
-    not_converge = True
     k = 0
-    k_old = 0.0
     lam = 0.001
+    not_converge = True
 
-    while not_converge:
-        not_converge = False
-        for i in range(n):
-            x = X[i, :]
-            xT = numpy.transpose([x])
-            # print('xT:\n', xT, xT.shape)
-            # print('theta shape', theta.shape)
-            y_hat = sigmoid(x, theta)
-            # print('y_hat:', y_hat)
-            beta = de_norm1(theta)
-
-            func_g = (y[i] - y_hat)*xT + lam*beta
-            
-            theta_k = theta.copy()
-            theta = theta + alpha*func_g
-            # print('theta shape:', theta.shape)
-            # print('k:', k)
-            for delta in abs(theta-theta_k):
-                if delta > eps:
-                    not_converge = True
-                    break
-
-        k+=1
-        if (k-k_old)>500:
-        	k_old = k
-        	print('process:', k/iters*100, '%')
-        if k > iters:
-            print('k > iters!')
+    for k in range(iters):
+        if not not_converge:
             break
+        i = k%n
+        x = X[i, :]
+        xT = numpy.transpose([x])
+        y_hat = sigmoid(x, theta)
+        # print('y_hat:', y_hat)
+        beta = de_norm1(theta)
+
+        func_g = (y[i] - y_hat)*xT + lam*beta
+        if i > n-20:
+            print('y_hat:', y_hat, 'y[i]:', y[i])
+            print('y_hat - y[i]:', y_hat - y[i])
+        
+        theta_k = theta.copy()
+        theta = theta + alpha*func_g
+        # print('theta shape:', theta.shape)
+        not_converge = True
+        for delta in abs(theta-theta_k):
+            if delta > eps:
+                converge = False
+                break
+
     # print('theta:\n', theta)
     return theta
 def de_norm1(theta):
@@ -81,9 +75,7 @@ def de_norm1(theta):
     for i in range(d):
         if theta[i,0] < 0:
             beta[i,0] = -1
-        elif theta[i,0] - 0 < 1e-5:
-            beta[i,0] = 0
-        else:
+        elif theta[i,0] > 0:
             beta[i,0] = 1
     return beta
 
@@ -92,41 +84,71 @@ def sigmoid(X, theta):
     value = 1.0/(1.0 + numpy.exp(-z))
     return value
 
-def predict(X, theta, threshold):
+def predict(X, y_true, theta, threshold):
     value = sigmoid(X, theta)
     # print('value:\n', value)
     row, col = X.shape
     y_hat = numpy.zeros(row)
+    P = sum(y_true)
+    N = row - P
+    FP = 0.0
+    TP = 0.0
+    print('P =', P)
+
     for i, val in enumerate(value):
         if val > threshold:
+            # print('> threshold', val)
             y_hat[i] = 1
+            TP+=1
         else:
             y_hat[i] = 0
-    return y_hat
+        if y_true[i] != y_hat[i]:
+            if y_true[i] == 0:
+                FP+=1
+    FPR = FP/N
+    TPR = TP/P
+    return y_hat, FPR, TPR
 
 # plot the ROC curve of your prediction
 # x aixes: TPR = TP / ( TP + FN )
 # y aixes: FPR = FP / ( FP + TN ) 
 def plot_roc_curve(X_test, y_true, theta):
     k = 51
-    TPR_x = numpy.zeros(k)
-    FPR_y = numpy.zeros(k)
+    FPR_x = numpy.zeros(k)
+    TPR_y = numpy.zeros(k)
 
     for n in range(k):
         threshold = n/(k-1)
         # print('threshold=', threshold)
-        y_pred = predict(X_test, theta, threshold)
-        tn, fp, fn, tp = sklearn.metrics.confusion_matrix(y_true, y_pred).ravel()
-        TPR_x[n] = tp/(tp+fn)
-        FPR_y[n] = fp/(fp+tn)
-        print(tn, fp, fn, tp)
-        print(TPR_x[n], FPR_y[n])
+        y_pred, FPR, TPR = predict(X_test, y_true, theta, threshold)
+        # tn, fp, fn, tp = sklearn.metrics.confusion_matrix(y_true, y_pred).ravel()
+        # FPR_x[n] = fp/(fp+tn)
+        # TPR_y[n] = tp/(tp+fn)
+        FPR_x[n] = FPR
+        TPR_y[n] = TPR
+        # print(tn, fp, fn, tp)
+        print(FPR_x[n], TPR_y[n])
 
-    plt.xlabel('TPR')
-    plt.ylabel('FPR')
-    plt.plot(TPR_x, FPR_y, '-')
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.plot(FPR_x, TPR_y, '-')
+    # plt.scatter(FPR_x, TPR_y, marker='o', color='blue')
     plt.show()
 
+def plot_sigmoid(X, theta, y_true=None):
+    n, d = X.shape
+    color = ['red', 'blue', 'yellow', 'green', 'black', 'gray', 'orange', 'purple', 'c']
+    # print(d)
+    y_pred = numpy.zeros((n, n))
+    
+    for i in range(d):
+        # xT = numpy.transpose([X[:, i]])
+        # y_pred[i] = sigmoid(xT, theta[i])
+        # print(y_pred[i].shape)
+        # print(X[:, i].shape)
+        plt.scatter(X[:, i], y_true, marker='o', color=color[i])
+
+    plt.show()
 
 def main(argv):
     X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.8)
@@ -135,8 +157,7 @@ def main(argv):
     theta = logreg_sgd(X_train_scale, y_train)
     print('theta:\n', theta)
     plot_roc_curve(X_test, y_test, theta)
-
-
+    # plot_sigmoid(X_test, theta, y_true=y_test)
 
 
 if __name__ == "__main__":
